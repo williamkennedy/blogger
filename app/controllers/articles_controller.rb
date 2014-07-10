43b10c240386192b990527953687a2fa3c2bfbd1
williamkennedy <williamkennedyjnr@gmail.com>
2014-07-10 16:23:39 +0000

    Adding stylsheet, added create function, but need to add delete function next

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,33 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
   @articles = Article.all
 end
   def show
   @article = Article.find(params[:id])
 end
+  def new
+  @article = Article.new
+end
+  ##This method was the original create method
+  #def create
+ #  @article = Article.new(
+ #    title: params[:article][:title],
+  #   body: params[:article][:body])
+  
+ # @article.save
+   # redirect_to article_path(@article)
+    
+#end
+  def create
+  @article = Article.new(article_params)
+  @article.save
+    
+  redirect_to article_path(@article)
+end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path(@article)
+  end
 end
