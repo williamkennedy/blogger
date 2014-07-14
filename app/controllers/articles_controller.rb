579c825f06ded783af2137abfddb6397e1bd2d23
williamkennedy <williamkennedyjnr@gmail.com>
2014-07-14 12:21:29 +0000

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,20 +23,32 @@ end
     
 #end
   def create
-  @article = Article.new(article_params)
+    @article = Article.new(article_params) #creating new article
   @article.save
+    flash.notice = "Article '#{@article.title}' Created!"#adding update message
     
   redirect_to article_path(@article)
 end
   
   def destroy
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id])#finding article by id
     @article.destroy
+    
+    flash.notice = "Article '#{@article.title}' Deleted!"#adding update message
     
     redirect_to articles_index_path
   end
   def edit
-    @article = Article.new(article_params)
+    @article = Article.find(params[:id])#finding article by id
+    
   end
+  def update
+    @article = Article.find(params[:id])#finding article by id
+  @article.update(article_params)
+    
+    flash.notice = "Article '#{@article.title}' Updated!"#adding update message
+
+  redirect_to article_path(@article)
+end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blogger::Application.routes.draw do
   get "articles/index"
+  
+  root to: 'articles#index'#making articles the main page
   resources :articles
 end
